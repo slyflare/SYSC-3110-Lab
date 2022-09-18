a491fc7d07925d59fc4299581a7fc18860fb7167
Vimal Gunasegaran <55605963+slyflare@users.noreply.github.com>
2022-09-18 11:58:09 -0400

    Added AddressBook and BuddyInfo

--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -4,17 +4,41 @@
 public class BuddyInfo {
 
     private String name;
+    private String address;
+    private String phoneNumber;
 
-    public BuddyInfo(String name) {
+    public BuddyInfo(String name, String address, String phoneNumber) {
         this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public static void main(String[] args) {
-        BuddyInfo p1 = new BuddyInfo("Dave");
+        BuddyInfo p1 = new BuddyInfo("Dave", "Carleton", "123");
         System.out.println("Hello " + p1.getName() + "!");
     }
 }
